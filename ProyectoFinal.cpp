2e597a2b49f84901bcc2a1e5a712dc37cd36640f
Eme112 <eme_pedraza@hotmail.com>
2023-05-25 18:53:34 -0600

    Added matrices and validated inputs

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -1,20 +1,75 @@
 // ProyectoFinal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Question: What happens if the size of the file is bigger than the input
+// (e.g. declaring a 3x3 matrix when the file contains 15 elements).
+// 
+// TODO: Review maximum size of memmory allignment.
+// TODO: Calculate result using sequential algorithm.
 
 #include <iostream>
+#include <stdio.h>
+
+// Matrix global var
+uint32_t rows_a, cols_a, rows_b, cols_b, rows_c, cols_c;
+double *A = NULL, *B= NULL, *C = NULL;
+
+uint8_t validateSize(int rows, int cols, char matrix) {
+    FILE* file;
+    errno_t err;
+    double value;
+    int count = 0;
+
+    // Open the file in read mode
+    if (matrix == 'A')
+        err = fopen_s(&file, "matrizA.txt", "r");
+    else
+        err = fopen_s(&file, "matrizB.txt", "r");
+
+    if (err != 0) {
+        printf("Failed to open the file.\n");
+        return 0; // Exit the program with an error
+    }
+
+    // Read double values until the end of the file
+    while (fscanf_s(file, "%lf", &value) != EOF) {
+        count++;
+        char nextChar = fgetc(file);
+    }
+
+    // Close the file
+    fclose(file);
+
+    // Output the count of double values
+    // printf("Number of lines: %d\n", count);
+
+    if (count == rows * cols) {
+        return count;
+    } else {
+        printf("Invalid size for matrix %c\n", matrix);
+        return 0;
+    }
+}
+
+void validateInputs() {
+    // Ask for inputs
+    printf("Enter number of rows for matrix A: ");
+    scanf_s("%d", &rows_a);
+    printf("Enter number of columns for matrix A: ");
+    scanf_s("%d", &cols_a);
+    printf("Enter number of rows for matrix B: ");
+    scanf_s("%d", &rows_b);
+    printf("Enter number of columns for matrix B: ");
+    scanf_s("%d", &cols_b);
+    // Validate inputs
+    if (validateSize(rows_a, cols_a, 'A') && validateSize(rows_b, cols_b, 'B')
+        && cols_a == rows_b)
+        printf("Valid multiplication\n");
+    else
+        printf("Multiplication not valid\n");
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    validateInputs();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
